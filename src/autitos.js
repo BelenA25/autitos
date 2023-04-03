0336e3706a52001c267ab5ccc7d5f53a6a60f623
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-04-03 19:47:49 -0400

    mejora de la primera prueba 2

--- src/autitos.js
+++ src/autitos.js
@@ -1,6 +1,6 @@
 function superficiePlana(dimension) {
   if(dimension=="") return "ingrese una cadena valida";
-  if(dimension.length == 1)  return convertirANumero(dimension);
+  if(dimension.length == 1)  return "la dimension necesita tener 2 valores ej; 2,2";
   if(dimension[0] != dimension[2]) return 0;
   else return convertirANumero(dimension[0])
 }
--- src/autitos.test.js
+++ src/autitos.test.js
@@ -4,8 +4,8 @@ describe("Superficie Plana retorno de dimension", () => {
   it("deberia retornar mensaje de error cuando la cadena es vacia", () => {
     expect(superficiePlana("")).toEqual("ingrese una cadena valida");
   });
-  it("deberia retornar el mismo numero con un numero", () => {
-    expect(superficiePlana("1")).toEqual(1);
+  it("deberia retornar un mensaje de error para la superficie de una dimension", () => {
+    expect(superficiePlana("1")).toEqual("la dimension necesita tener 2 valores ej; 2,2");
   });
   it("deberia retornar 0 con dimensiones no cuadradas", () => {
     expect(superficiePlana("1,2")).toEqual(0);
