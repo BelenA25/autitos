54552d5407ca4fd231ef99fd6e7921b6954e5758
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-04-03 19:51:34 -0400

    mejora de la tercera prueba deberia retornar un error de para las dimensiones no cuadradas

--- src/autitos.js
+++ src/autitos.js
@@ -1,7 +1,7 @@
 function superficiePlana(dimension) {
   if(dimension=="") return "ingrese una cadena valida";
-  if(dimension.length == 1)  return "la dimension necesita tener 2 valores ej; 2,2 ";
-  if(dimension[0] != dimension[2]) return 0;
+  if(dimension.length == 1)  return "la dimension necesita tener 2 valores ej; 2,2";
+  if(dimension[0] != dimension[2]) return "ingrese una dimension cuadrada";
   else return convertirANumero(dimension[0])
 }
 
--- src/autitos.test.js
+++ src/autitos.test.js
@@ -7,8 +7,8 @@ describe("Superficie Plana retorno de dimension", () => {
   it("deberia retornar un mensaje de error para la superficie de una dimension", () => {
     expect(superficiePlana("1")).toEqual("la dimension necesita tener 2 valores ej; 2,2");
   });
-  it("deberia retornar 0 con dimensiones no cuadradas", () => {
-    expect(superficiePlana("1,2")).toEqual(0);
+  it("deberia retornar mensaje de error con dimensiones no cuadradas", () => {
+    expect(superficiePlana("1,2")).toEqual("ingrese una dimension cuadrada");
   });
   it("deberia retornar la dimension cuando se ingresa correctamente", () => {
     expect(superficiePlana("4,4")).toEqual(4);
