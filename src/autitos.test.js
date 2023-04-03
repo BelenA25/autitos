ab32c96e87f099561111d4ddfcd07e82bf0b7778
“Belen <beckiar23@gmail.com>
2023-04-02 22:04:38 -0400

    Prueba de retorno con dimensiones correctas

--- src/autitos.test.js
+++ src/autitos.test.js
@@ -10,4 +10,7 @@ describe("Superficie Plana retorno de dimension", () => {
   it("deberia retornar 0 con dimensiones no cuadradas", () => {
     expect(superficiePlana("1,2")).toEqual(0);
   });
+  it("deberia retornar la dimension cuando se ingresa correctamente", () => {
+    expect(superficiePlana("4,4")).toEqual(4);
+  });
 });
