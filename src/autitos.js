e4760ec35f7844d681306e301308b37c9f76f7cd
Jacco126 <jonatan.terceros@ucb.edu.bo>
2023-04-03 20:05:45 -0400

    se mejoro los mensajes de la interfaz de usuario

--- src/autitos.js
+++ src/autitos.js
@@ -1,8 +1,8 @@
 function superficiePlana(dimension) {
-  if(dimension=="") return "ingrese una cadena valida";
-  if(dimension.length == 1)  return "la dimension necesita tener 2 valores ej; 2,2";
-  if(dimension[0] != dimension[2]) return "ingrese una dimension cuadrada";
-  else return dimension
+  if(dimension=="") return "Ingrese una cadena valida";
+  if(dimension.length == 1)  return "La dimension necesita tener 2 valores ej; 2,2";
+  if(dimension[0] != dimension[2]) return "Ingrese una dimension cuadrada";
+  else return "La dimension es: " + dimension
 }
 
 export default superficiePlana;
