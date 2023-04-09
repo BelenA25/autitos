f47a3096c834a82657f80c42643f9ed175de03d7
“Belen <beckiar23@gmail.com>
2023-04-09 17:45:05 -0400

    Mejoras de pruebas

--- src/autitos.js
+++ src/autitos.js
@@ -1,8 +1,10 @@
+//int [][] ventas = new int[4][6];
+
 function superficiePlana(dimension) {
   if(dimension=="") return "Ingrese una cadena valida";
   if(dimension.length == 1)  return "La dimension necesita tener 2 valores ej; 2,2";
   if(dimension[0] != dimension[2]) return "Ingrese una dimension cuadrada";
-  else return "La dimension es: " + dimension
+  else return dimension
 }
 
 export default superficiePlana;
--- src/autitos.test.js
+++ src/autitos.test.js
@@ -2,13 +2,13 @@ import superficiePlana from "./autitos.js";
 
 describe("Superficie Plana retorno de dimension", () => {
   it("deberia retornar mensaje de error cuando la cadena es vacia", () => {
-    expect(superficiePlana("")).toEqual("ingrese una cadena valida");
+    expect(superficiePlana("")).toEqual("Ingrese una cadena valida");
   });
   it("deberia retornar un mensaje de error para la superficie de una dimension", () => {
-    expect(superficiePlana("1")).toEqual("la dimension necesita tener 2 valores ej; 2,2");
+    expect(superficiePlana("1")).toEqual("La dimension necesita tener 2 valores ej; 2,2");
   });
   it("deberia retornar mensaje de error con dimensiones no cuadradas", () => {
-    expect(superficiePlana("1,2")).toEqual("ingrese una dimension cuadrada");
+    expect(superficiePlana("1,2")).toEqual("Ingrese una dimension cuadrada");
   });
   it("deberia retornar la dimension cuando se ingresa correctamente", () => {
     expect(superficiePlana("4,4")).toEqual("4,4");
