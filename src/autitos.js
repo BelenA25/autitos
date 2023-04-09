67c2a54fb34a82ffb6dd2ca149c562797e181a76
“Belen <beckiar23@gmail.com>
2023-04-09 18:00:49 -0400

    Creacion de la matriz que represente la superficie plana

--- src/autitos.js
+++ src/autitos.js
@@ -1,10 +1,21 @@
-//int [][] ventas = new int[4][6];
+let matriz = [[]];
 
 function superficiePlana(dimension) {
   if(dimension=="") return "Ingrese una cadena valida";
   if(dimension.length == 1)  return "La dimension necesita tener 2 valores ej; 2,2";
   if(dimension[0] != dimension[2]) return "Ingrese una dimension cuadrada";
-  else return dimension
+  else{
+    matriz = crearMatriz(parseInt(dimension[0]));
+    return dimension;
+  }
+}
+
+function crearMatriz(n) {
+  const matriz = [];
+  for (let i = 0; i < n; i++) {
+    matriz.push(Array(n).fill(0));
+  }
+  return matriz;
 }
 
 export default superficiePlana;
